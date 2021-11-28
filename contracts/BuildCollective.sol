54ba8576f671835950083b09ced991cc9f012f5c
WassimAROUI-NazimBENRABAH <projet2daar@gmail.com>
2021-11-28 21:37:43 +0100

    master

--- contracts/BuildCollective.sol
+++ contracts/BuildCollective.sol
@@ -9,8 +9,99 @@ contract BuildCollective is Ownable {
     uint256 balance;
     bool registered;
   }
+  struct Entreprise {
+    string ENT_name;
+    User Owner;
+    User [] members;
+    uint256 balance;
+    bool registered;
+  }
+  struct Project {
+    string name;
+    string Owner;
+    User [] Contributers;
+    uint256 balance;
+    bool registered;
+  }
 
-  mapping(address => User) private users;
+  
+
+  mapping(address => User) public users;
+  mapping(address => Entreprise) private ents;
+  mapping(address => Project) private projects;
+
+
+
+
+  // Create User 
+  
+  function createUser(address _userAddress, uint256 _balance,string memory _userName) public {
+    User storage user = users[_userAddress];
+    // Check that the user did not already exist:
+    require(!user.registered);
+    //Store the user
+    users[_userAddress].balance =_balance;
+    users[_userAddress].username =_userName;
+    //require(keccak256(abi.encodePacked((users[_userAddress].username)))==keccak256(abi.encodePacked((_userName))),"NON");
+    users[_userAddress].registered= true; 
+    //item++;
+    
+}
+
+// Create Entreprise
+function createEnt(address _userAddress, uint256 _balance,string memory _entName,User[] memory _members,User memory _owner) public {
+    Entreprise storage ent = ents[_userAddress];
+    // Check that the user did not already exist:
+    require(!ent.registered);
+    //Store the user
+    ents[_userAddress].balance= _balance;
+    ents[_userAddress].ENT_name= _entName;
+    ents[_userAddress].Owner= _owner;
+    uint256 len=_members.length;
+    for (uint256 i = 0; i < len; i+=1) {
+            ents[_userAddress].members.push(_members[i]);
+        }
+    ents[_userAddress].registered= true;
+    
+}
+
+// Create Project
+function createProject(address _userAddress, uint256 _balance,string memory _Name,User[] memory _cont,string memory _owner) public {
+    Project storage pro = projects[_userAddress];
+    // Check that the user did not already exist:
+    require(!pro.registered);
+    //Store the user
+    projects[_userAddress].balance= _balance;
+    projects[_userAddress].name= _Name;
+    projects[_userAddress].Owner= _owner;
+    uint256 len=_cont.length;
+    for (uint256 i = 0; i < len; i+=1) {
+            projects[_userAddress].Contributers.push(_cont[i]);
+        }
+    projects[_userAddress].registered= true;
+}
+
+// send ether
+ function sendEther (address _to) public payable {
+   address payable __to = address(uint160(_to));
+   (bool sent,bytes memory data)=__to.call.value(msg.value)("");
+   require(sent,"Failed to send Ether");
+
+}
+// send ether for project
+  function sendEther_project (address _to) public {
+   bool exist=false;
+   uint256 len=projects[address (this)].Contributers.length;
+   for (uint256 i=0;i<len;i+=1){
+     if (keccak256 (abi.encodePacked(projects[address (this)].Contributers[i].username,projects[address (this)].Contributers[i].balance,projects[address (this)].Contributers[i].registered))==keccak256 (abi.encodePacked(users[_to].username,users[_to].balance,users[_to].registered))){
+       exist=true;
+       break;
+     }
+   }
+   require(exist,"The account is not a contributer of a project");
+   address payable __to = address(uint160(_to));
+   sendEther(__to);
+}
 
   event UserSignedUp(address indexed userAddress, User indexed user);
 
--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   networks: {
     development: {
-      host: '127.0.0.1',
+      host: '10.188.110.97',
       port: 7545,
       network_id: '*',
     },
     test: {
-      host: '127.0.0.1',
+      host: '10.188.110.97',
       port: 7545,
       network_id: '*',
     },
