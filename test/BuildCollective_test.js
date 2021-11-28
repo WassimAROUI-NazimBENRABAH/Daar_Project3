e447900121c0a4c0ed854d3ef2b8d58dcb665dea
WassimAROUI-NazimBENRABAH <projet2daar@gmail.com>
2021-11-28 21:39:16 +0100

    hada

---
+++ test/BuildCollective_test.js
@@ -0,0 +1,15 @@
+const buildcollective = artifacts.require('BuildCollective');
+
+contract('BuildCollective',function(accounts) {
+let buildcont=null;
+before(async() => { buildcont = await buildcollective.deployed();});
+
+it('Should create a new user account ', async () => {
+   // var ad = await accounts[0];
+    //console.log(ad);
+   //sconsole.log(accounts[1]);
+    await buildcont.sendEther(accounts[0]);
+    //console.log(u);
+    //assert(u.username === 'Wassim');
+} );
+});
