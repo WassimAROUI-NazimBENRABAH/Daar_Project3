30cf8b6c5e33882e5f6960f9e544cc720da8fae5
WassimAROUI-NazimBENRABAH <projet2daar@gmail.com>
2021-11-28 21:50:59 +0100

    seconde commit

--- test/BuildCollective_test.js
+++ test/BuildCollective_test.js
@@ -4,11 +4,11 @@ contract('BuildCollective',function(accounts) {
 let buildcont=null;
 before(async() => { buildcont = await buildcollective.deployed();});
 
-it('Should create a new user account ', async () => {
+it('Should create exchange ethers', async () => {
    // var ad = await accounts[0];
     //console.log(ad);
    //sconsole.log(accounts[1]);
-    await buildcont.sendEther(accounts[0]);
+    await buildcont.sendEther(accounts[1]);
     //console.log(u);
     //assert(u.username === 'Wassim');
 } );
